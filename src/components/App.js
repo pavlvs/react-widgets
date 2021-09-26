1b04ce780eec4adc88b32c1fbc9275d2bf935a4a
pavlvs <pavlvsxavier@gmail.com>
2021-09-26 17:25:24 +0100

    project setup

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+    state = {}
+
+    render() {
+        return (
+            <div className='ui container'>
+                App
+            </div>
+        )
+    }
+}
+
+export default App
