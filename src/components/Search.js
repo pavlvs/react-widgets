7cff1f8a89ee52363bb9df1ed91cb9b940b8e183
pavlvs <pavlvsxavier@gmail.com>
2021-09-27 18:52:44 +0100

    fixed results.length dependency warning with debouncedTerm and two useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,17 @@ import axios from 'axios'
 
 const Search = () => {
     const [term, setTerm] = useState('programming')
+    const [debouncedTerm, setDebouncedTerm] = useState(term)
     const [results, setResults] = useState([])
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term)
+        }, 1000)
+        return () => {
+            clearTimeout(timerId)
+        }
+    }, [term])
 
     useEffect(() => {
         const search = async () => {
@@ -15,7 +25,7 @@ const Search = () => {
                         list: 'search',
                         origin: '*',
                         format: 'json',
-                        srsearch: term,
+                        srsearch: debouncedTerm,
                     },
                 }
             )
@@ -23,20 +33,10 @@ const Search = () => {
             setResults(data.query.search)
         }
 
-        if (term && !results.length) {
+        if (debouncedTerm) {
             search()
-        } else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    search()
-                }
-            }, 500)
-
-            return () => {
-                clearTimeout(timeoutId)
-            }
         }
-    }, [term])
+    }, [debouncedTerm])
 
     const renderedResults = results.map((result) => {
         return (
