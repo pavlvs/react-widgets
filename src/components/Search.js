2248091d1a673d2f14e3773e95b6614cfef0cd0b
pavlvs <pavlvsxavier@gmail.com>
2021-09-27 16:37:58 +0100

    retrieved search term results with axiosand stored it using useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 const Search = () => {
+    const [term, setTerm] = useState('programming')
+    const [results, setResults] = useState([])
+
+    useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get(
+                'https://en.wikipedia.org/w/api.php',
+                {
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        origin: '*',
+                        format: 'json',
+                        srsearch: term,
+                    },
+                }
+            )
+
+            setResults(data.query.search)
+        }
+        search()
+        return () => {}
+    }, [term])
     return (
         <div>
-            <h1>Search</h1>
+            <div className='ui form'>
+                <div className='field'>
+                    <label htmlFor=''>Enter Search Term</label>
+                    <input
+                        value={term}
+                        onChange={(e) => setTerm(e.target.value)}
+                        type='text'
+                        className='input'
+                    />
+                </div>
+            </div>
         </div>
     )
 }
