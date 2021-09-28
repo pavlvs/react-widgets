ef2aa9c059b80038944c19ca649c379840ff8153
pavlvs <pavlvsxavier@gmail.com>
2021-09-28 21:21:35 +0100

    added navigation header

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import Accordion from './Accordion'
 import Search from './Search'
 import Dropdown from './Dropdown'
 import Translate from './Translate'
+import Route from './Route'
+import Header from './Header'
 
 // import Accordion from './Accordion'
 
@@ -37,34 +39,28 @@ const options = [
     },
 ]
 
-const showAccordion = () => {
-    if (window.location.pathname === '/') {
-        return <Accordion items={items} />
-    }
-}
-const showList = () => {
-    if (window.location.pathname === '/list') {
-        return <Search />
-    }
-}
-const showDropdown = () => {
-    if (window.location.pathname === '/dropdown') {
-        return <Dropdown />
-    }
-}
-const showTranslate = () => {
-    if (window.location.pathname === '/translate') {
-        return <Translate />
-    }
-}
-
 export default () => {
+    const [selected, setSelected] = useState(options[0])
     return (
         <div>
-            {showAccordion()}
-            {showList()}
-            {showDropdown()}
-            {showTranslate()}
+            <Header />
+            <Route path='/'>
+                <Accordion items={items} />
+            </Route>
+            <Route path='/list'>
+                <Search />
+            </Route>
+            <Route path='/dropdown'>
+                <Dropdown
+                    label='Select a color'
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path='/translate'>
+                <Translate />
+            </Route>
         </div>
     )
 }
