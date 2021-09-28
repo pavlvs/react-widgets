98c471854324b066a64d9b717196929c43ab9f65
pavlvs <pavlvsxavier@gmail.com>
2021-09-28 09:20:25 +0100

    changed selection state on clicking on an option

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-const Dropdown = () => {
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const renderedOptions = options.map((option) => {
+        return (
+            <div
+                key={option.value}
+                className='item'
+                onClick={() => onSelectedChange(option)}>
+                {option.label}
+            </div>
+        )
+    })
     return (
-        <div>
-            <h1>Dropdown</h1>
+        <div className='ui form'>
+            <div className='field'>
+                <label htmlFor='' className='label'>
+                    Select a Color
+                </label>
+                <div className='ui selection dropdown visible active'>
+                    <i className='dropdown icon'></i>
+                    <div className='text'>{selected.label}</div>
+                    <div className='menu visible transition'>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
